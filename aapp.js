0e4f424ac375d4db753804ea65be3f20b0ee9d37
Victorio <victoriocaccioppoli@gmail.com>
2023-06-16 18:31:09 -0300

    finished

---
+++ aapp.js
@@ -0,0 +1,48 @@
+function noFunciona(){
+    useCallback(
+      () => {
+        first
+      },
+      [second],
+    )
+    
+}
+import React from 'react'
+
+function 
+import React, { Component } from 'react'
+
+export default class aapp extends Component {
+  render() {
+    return (
+      <div>aapp</div>
+    )
+  }
+}
+() {
+  return (
+    <div>
+        import React, { Component } from 'react'
+        
+        export default class aapp extends Component {
+          render() {
+            return (
+              <div>aapp</div>
+            )
+          }
+        }
+        
+    </div>
+  )
+}
+
+export default 
+import React, { Component } from 'react'
+
+export default class aapp extends Component {
+  render() {
+    return (
+      <div>aapp</div>
+    )
+  }
+}
